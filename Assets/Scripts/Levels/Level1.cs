ff85b3d578363b81509e724c61f77a8dd99b0fd8
jakob <jakob.willforss@hotmail.com>
2016-12-14 21:28:26 +0100

    First basic spawn

---
+++ Assets/Scripts/Levels/Level1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Levels;
+using System.Collections.Generic;
+
+public class Level1 : MonoBehaviour {
+
+    public GameObject enemy_prefab;
+
+    private List<Spawn> spawns;
+
+    private GameObjects game_objects;
+
+	void Start () {
+        spawns = new List<Spawn>();
+        game_objects = GetComponent<GameObjects>();
+
+        Spawn spawn = new Spawn(enemy_prefab, 1);
+
+        spawns.Add(spawn);
+	}
+	
+	void Update () {
+	    foreach (Spawn spawn in spawns) {
+            spawn.MyUpdate();
+        }
+	}
+}
---
+++ Assets/Scripts/Levels/Spawn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Levels {
+    class Spawn : MonoBehaviour {
+
+        private float spawn_time;
+        private GameObject enemy_object;
+        private bool is_used;
+
+        public Spawn(GameObject enemy_object, float time) {
+
+            this.enemy_object = enemy_object.gameObject;
+            spawn_time = Time.time + time;
+
+            is_used = false;
+
+            print("Spawn at" + spawn_time);
+
+        }
+
+        public void MyUpdate() {
+
+            if (spawn_time > Time.time && !is_used) {
+
+                print(enemy_object);
+
+                Instantiate(enemy_object);
+                is_used = true;
+            }
+        }
+    }
+}
